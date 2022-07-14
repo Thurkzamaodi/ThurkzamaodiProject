6b32a6f3d16de78c0f5b76f8260cc175bb366617
Samati Ongartchai <108978338+Thurkzamaodi@users.noreply.github.com>
2022-07-14 21:48:56 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("Hello, world")
+
+balance = 30000; 
+withDraw = 25000; 
+console.log('Your amount is: ', balance 'Baht');
+if (withDraw <= balance) {
+  console.log('Withdraw amount: ', withDraw, 'Baht');
+  balance -= withDraw
+  console.log('Balance amount: ', balance, 'Baht');
+} else if (withDraw >= balance) {
+  console.log("You don't have money.");
+}
+  
